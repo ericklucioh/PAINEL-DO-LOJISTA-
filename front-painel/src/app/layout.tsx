16ef352284f46a2250201bbf46cc3f4fc03a6419
ericklucioh <erickrealoficial@gmail.com>
2026-05-27 19:23:34 -0300

    [main] isntalando shadcn

--- front-painel/src/app/layout.tsx
+++ front-painel/src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Painel do Lojista",
@@ -12,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR" className="h-full antialiased">
+    <html lang="pt-BR" className={cn("h-full antialiased", "font-sans", geist.variable)}>
       <body className="min-h-full bg-[radial-gradient(circle_at_top,_rgba(255,255,255,0.95),_rgba(241,245,249,1)_42%,_rgba(226,232,240,1)_100%)] text-slate-950">
         {children}
       </body>
